b2395d01c8e46cbe522c2b6e97dfadd163149785
Bion Oren <bion@drewcrawfordapps.com>
2011-09-14 15:47:04 -0500

    Added an installer

---
+++ setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+setup(name='lint.py',version='1.0',
+py_modules=['SourceFile','objCAuditer','objCProperty','ReSubLogger'],
+scripts=['lint.py'],
+data_files = []
+)
