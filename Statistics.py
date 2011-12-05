e1b6f3a562188dd35e309894fbcbf9b30e183ef2
Drew Crawford <drew@sealedabstract.com>
2011-12-04 22:59:01 -0600

    fixing installation

--- Statistics.py
+++ Statistics.py
@@ -1,3 +1,4 @@
+#!/bin/python
 import re
 from SourceFile import SourceFile
 
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='lint.py',version='1.0',
-py_modules=['SourceFile','objCAuditer','objCProperty','ReSubLogger'],
+py_modules=['SourceFile','objCAuditer','objCProperty','ReSubLogger','Statistics'],
 scripts=['lint.py'],
 data_files = []
 )
